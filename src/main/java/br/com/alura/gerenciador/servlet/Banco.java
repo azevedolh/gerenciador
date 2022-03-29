517b76324d9bc0600b5af56c2cfc7d3aff44c960
Luan Azevedo <azeveedolh@gmail.com>
2022-03-28 21:08:36 -0300

    Implementing remove function on mock database

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -3,24 +3,37 @@ package br.com.alura.gerenciador.servlet;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Banco {
-	
+
 	static private List<Empresa> listaEmpresas = new ArrayList<Empresa>();
-	
+
+	private static Integer chaveSequencial = 1;
+
 	static {
 		Empresa empresa1 = new Empresa("Alura", new Date());
 		Empresa empresa2 = new Empresa("Caelum", new Date());
+		empresa1.setId(Banco.chaveSequencial++);
 		Banco.listaEmpresas.add(empresa1);
+		empresa2.setId(Banco.chaveSequencial++);
 		Banco.listaEmpresas.add(empresa2);
 	}
-	
+
 	public List<Empresa> getEmpresas() {
 		return Banco.listaEmpresas;
 	}
-	
+
 	public void adicionar(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.listaEmpresas.add(empresa);
 	}
 
+	public void remover(Integer id) {
+		List<Empresa> empresas = Banco.listaEmpresas.stream()
+				.filter(e -> e.getId().equals(id))
+				.limit(1)
+				.collect(Collectors.toList());
+		Banco.listaEmpresas.remove(empresas.get(0));
+	}
 }
